84037a4664fe12e1b9a7d4c795eac9c4b1dec092
Mrlonely04 <gearlock60@fastmail.com>
2026-06-16 17:41:29 -0400

    update

--- home.nix
+++ home.nix
@@ -39,6 +39,11 @@
    };
       programs.obs-studio = {
          enable = true;
+         package = (
+            pkgs.obs-studio.override {
+               cudaSupport = true;
+            }
+         );
          plugins = with pkgs.obs-studio-plugins; [
             obs-backgroundremoval
             obs-pipewire-audio-capture
