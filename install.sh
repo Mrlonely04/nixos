20540a057c2da7550de6d14997a760c28b34e7d0
Mrlonely04 <gearlock60@fastmail.com>
2026-08-06 11:42:16 -0400

    added a simple install scripts

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p bash
+
+echo "what system do you want to install? desktop, laptop, homelab, steamMachine."
+read system
+
+echo "building $system!"
+
+sudo nixos-rebuild switch --flake ~/nixos-dotfiles#$system
+
