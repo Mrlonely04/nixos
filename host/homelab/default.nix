691558f606187ce649fb54095e54c2d6404383de
Mrlonely04 <gearlock60@fastmail.com>
2026-08-11 14:20:57 -0400

    added docker

--- host/homelab/default.nix
+++ host/homelab/default.nix
@@ -4,6 +4,7 @@
       [ # Include the results of the hardware scan.
          ./hardware-configuration.nix
          ./../../modules/core
+         ./../../modules/common/docker
       ];
 
    # Defines devices name
